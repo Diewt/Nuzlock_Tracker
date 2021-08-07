7b804034b5c3e49681ce11034a94672781984993
Stone Sha <36431006+stonesha@users.noreply.github.com>
2021-08-07 10:43:45 -0700

    overhaul to react/nextjs

---
+++ next.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    future: {
+        strictPostcssConfiguration: true
+    },
+    reactStrictMode: true,
+    images: {
+        domains: [
+            'pokeapi.co', // pokeAPI sprites
+        ]
+    },
+    webpack5: false
+};
