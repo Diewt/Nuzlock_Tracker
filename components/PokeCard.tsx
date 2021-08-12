dd6c3ed9aa66a964b43f35eba3f8766f749f6669
Stone Sha <36431006+stonesha@users.noreply.github.com>
2021-08-12 00:24:55 -0700

    select options include sprite

--- components/PokeCard.tsx
+++ components/PokeCard.tsx
@@ -1,9 +1,12 @@
 import { useState } from "react";
 import { AsyncPaginate } from "react-select-async-paginate";
 
-export default function PokeCard({ pokemonOptions }) {
+export default function PokeCard({ pokemonOptions, pokemonSprites }) {
+
+    // placeholders for later implementation of select functionality
     const [value, onChange] = useState(null);
 
+    // function for filtering options when searching pokemon
     const loadOptions = async (search, prevOptions) => {
 
         let filteredOptions;
@@ -29,6 +32,14 @@ export default function PokeCard({ pokemonOptions }) {
         };
     };
 
+    // styling for the pokemon select options
+    const formatOptionLabel = ({ value, label }) => (
+        <div className="flex flex-row -my-2.5">
+            <img alt={value} src={pokemonSprites[value]} className="w-16 h-16" />
+            <p className="mt-4">{label}</p>
+        </div>
+    );
+
     return (
         <div className='p-4'>
             <div className='box-border border-2 border-solid shadow-lg grid grid-cols-12 gap-1.5 row-auto max-w-md dark:bg-gray-600 dark:border-yellow-500 mt-8 p-2 relative rounded'>
@@ -38,6 +49,7 @@ export default function PokeCard({ pokemonOptions }) {
                 </label>
                 <div className='col-span-5 row-span-1' />
                 <AsyncPaginate
+                    formatOptionLabel={formatOptionLabel}
                     value={value}
                     loadOptions={loadOptions}
                     onChange={onChange}
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,7 @@ import Container from '@/components/Container';
 import PokeCard from '@/components/PokeCard';
 import pokedex from '@/lib/pokeapi';
 
-const pokeSpriteUrl = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-viii/icons/";
+const pokeSpriteUrl = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/";
 
 interface Pokemon {
 	name: string,
@@ -19,12 +19,12 @@ interface PokemonOption {
 export const getStaticProps: GetStaticProps = async () => {
 	const pokemonList: Pokemon[] = (await pokedex.getPokemonsList()).results;
 
-	const pokemonSprites: string[] = [];
+	const pokemonSprites = {}
 	const pokemonOptions: PokemonOption[] = pokemonList.map(pokemon => {
 		const name: string = pokemon.name;
 		const spriteNum: string = pokemon.url.substring(pokemon.url.slice(0, -1).lastIndexOf('/') + 1).slice(0, -1);
 		const spriteUrl: string = pokeSpriteUrl + spriteNum + ".png";
-		pokemonSprites.push(spriteUrl);
+		pokemonSprites[name] = spriteUrl;
 		const pokemonOption: PokemonOption = { value: name, label: name.charAt(0).toUpperCase() + name.slice(1) }
 
 		return pokemonOption
@@ -38,11 +38,11 @@ export const getStaticProps: GetStaticProps = async () => {
 	}
 }
 
-export default function Home({ pokemonOptions }) {
+export default function Home({ pokemonOptions, pokemonSprites }) {
 	return (
 		<Container>
-			<PokeCard pokemonOptions={pokemonOptions} />
-			<PokeCard pokemonOptions={pokemonOptions} />
+			<PokeCard pokemonOptions={pokemonOptions} pokemonSprites={pokemonSprites} />
+			<PokeCard pokemonOptions={pokemonOptions} pokemonSprites={pokemonSprites} />
 		</Container>
 	)
 }
