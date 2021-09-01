4dae141dcb88174801d202627167c7ab1ff70618
Chris Diewtragulchai <44313901+Diewt@users.noreply.github.com>
2021-08-31 17:31:03 -0700

    reverted stat input changes since I felt it didn't make sense in terms of actual calcuation also added notes for function to add in later

--- components/PokeCard.tsx
+++ components/PokeCard.tsx
@@ -152,6 +152,126 @@ export default function PokeCard({ cardIndex, options, sprites }: PokeCardProps)
         setUserPokemonInfo(prevState => { return { ...prevState, moves: [...newMoves] } });
     }
 
+    //Plan for these two function is to later implement a button to switch display from base stats and calculated stats
+    //Initial blueprint is to store the calculated stats into the userPokemonInfo 
+    //Also to add bool flag to represent what stats should be displayed
+    //Tie that element to a button somewhere on the card.
+    //Before doing that we also need to figure out a way to get input for iv and evs for proper stat calculation
+    function statCalculation(index) {
+        return pokemonInfo.stats[index].base_stat
+
+        //store calculation into appropriate slot in userPokemon info
+        //stat calculation for hp for gen 3 onward
+        //Math.floor((2 * pokemonInfo.stats[index].base_stat + iv + Math.floor(ev/4) * userPokemonInfo.lvl)/100) + userPokemonInfo.lvl + 10
+
+        //stat calcualtion for other stats for gen 3 onward
+        //Math.floor((Math.floor((2 * pokemonInfo.stats[index].base_stat + iv + Math.floor(ev/4) * userPokemonInfo.lvl)/100) + 5) * natureBonus(index))
+    }
+
+    //Incredibly rough implementation of how to check the nature bonuses since we are storing natures by name
+    //Review later for improvements
+    function natureBonus(index) {
+        //neutral natures meaning no stat impact
+        if (userPokemonInfo.nature == 'hardy' ||
+            userPokemonInfo.nature == 'docile' ||
+            userPokemonInfo.nature == 'serious' ||
+            userPokemonInfo.nature == 'bashful' ||
+            userPokemonInfo.nature == 'quirky'
+        ){
+            return 1;
+        }
+
+        //natures affecting attack
+        if (index == 1){
+            if (userPokemonInfo.nature == 'lonely' ||
+                userPokemonInfo.nature == 'brave' ||
+                userPokemonInfo.nature == 'adamant' ||
+                userPokemonInfo.nature == 'naughty'
+            ){
+                return 1.1;
+            }
+            else if (userPokemonInfo.nature == 'bold' ||
+                    userPokemonInfo.nature == 'timid' ||
+                    userPokemonInfo.nature == 'modest' ||
+                    userPokemonInfo.nature == 'calm'
+            ){
+                return 0.9
+            }
+        }
+
+        //natures affecting defense
+        if (index == 2) {
+            if (userPokemonInfo.nature == 'bold' ||
+                userPokemonInfo.nature == 'relaxed' ||
+                userPokemonInfo.nature == 'impish' ||
+                userPokemonInfo.nature == 'lax'
+            ) {
+                return 1.1;
+            }
+            else if (userPokemonInfo.nature == 'lonely' ||
+                userPokemonInfo.nature == 'hasty' ||
+                userPokemonInfo.nature == 'mild' ||
+                userPokemonInfo.nature == 'gentle'
+            ) {
+                return 0.9
+            }
+        }
+
+        //natures affecting special attack
+        if (index == 3) {
+            if (userPokemonInfo.nature == 'modest' ||
+                userPokemonInfo.nature == 'mild' ||
+                userPokemonInfo.nature == 'quiet' ||
+                userPokemonInfo.nature == 'rash'
+            ) {
+                return 1.1;
+            }
+            else if (userPokemonInfo.nature == 'adamant' ||
+                userPokemonInfo.nature == 'impish' ||
+                userPokemonInfo.nature == 'jolly' ||
+                userPokemonInfo.nature == 'careful'
+            ) {
+                return 0.9
+            }
+        }
+
+        //natures affecting special defense
+        if (index == 4) {
+            if (userPokemonInfo.nature == 'calm' ||
+                userPokemonInfo.nature == 'gentle' ||
+                userPokemonInfo.nature == 'sassy' ||
+                userPokemonInfo.nature == 'careful'
+            ) {
+                return 1.1;
+            }
+            else if (userPokemonInfo.nature == 'rash' ||
+                userPokemonInfo.nature == 'naive' ||
+                userPokemonInfo.nature == 'lax' ||
+                userPokemonInfo.nature == 'naughty'
+            ) {
+                return 0.9
+            }
+        }
+
+        //natures affecting speed
+        if (index == 5) {
+            if (userPokemonInfo.nature == 'timid' ||
+                userPokemonInfo.nature == 'hasty' ||
+                userPokemonInfo.nature == 'jolly' ||
+                userPokemonInfo.nature == 'naive'
+            ) {
+                return 1.1;
+            }
+            else if (userPokemonInfo.nature == 'brave' ||
+                userPokemonInfo.nature == 'relaxed' ||
+                userPokemonInfo.nature == 'quiet' ||
+                userPokemonInfo.nature == 'sassy'
+            ) {
+                return 0.9
+            }
+        }
+    }
+
     return (
         <div className='p-4'>
             <div className='box-border border-2 border-solid shadow-lg grid grid-cols-12 gap-1.5 row-auto max-w-xl dark:bg-gray-600 dark:border-yellow-500 mt-8 p-2 relative rounded'>
@@ -227,49 +347,31 @@ export default function PokeCard({ cardIndex, options, sprites }: PokeCardProps)
                     </div>
                 }
                 <div className='col-span-7 row-span-6 box-border border-2 p-1 rounded-lg'>
-                    <div className='grid grid-cols-2 gap-y-2'>
+                    <div className='grid grid-cols-7 row-span-6 gap-y-2'>
 
-                        <label>Hp</label>
-                        <input
-                            className="w-9 place-self-end text-right"
-                            value={pokemonInfo ? pokemonInfo?.stats[0].base_stat : 0}
-                            name="hp"
-                        />
+                        <label className='col-span-1'>Hp</label>
+                        <div className='col-span-5'> bar placeholder </div>
+                        <div className='col-span-1 place-self-end'> {pokemonInfo ? statCalculation(0) : 0} </div>
 
-                        <label>Atk</label>
-                        <input
-                            className="w-9 place-self-end text-right"
-                            value={pokemonInfo ? pokemonInfo?.stats[1].base_stat : 0}
-                            name="atk"
-                        />
+                        <label className='col-span-1'>Atk</label>
+                        <div className='col-span-5'>bar placeholder</div>
+                        <div className='col-span-1 place-self-end'> {pokemonInfo ? statCalculation(1) : 0} </div>
 
-                        <label>Def</label>
-                        <input
-                            className="w-9 place-self-end text-right"
-                            value={pokemonInfo ? pokemonInfo?.stats[2].base_stat : 0}
-                            name="def"
-                        />
+                        <label className='col-span-1'>Def</label>
+                        <div className='col-span-5'>bar placeholder</div>
+                        <div className='col-span-1 place-self-end'> {pokemonInfo ? statCalculation(2) : 0} </div>
 
-                        <label>Spa</label>
-                        <input
-                            className="w-9 place-self-end text-right"
-                            value={pokemonInfo ? pokemonInfo?.stats[3].base_stat : 0}
-                            name="spa"
-                        />
+                        <label className='col-span-1'>Spa</label>
+                        <div className='col-span-5'>bar placeholder</div>
+                        <div className='col-span-1 place-self-end'> {pokemonInfo ? statCalculation(3) : 0} </div>
 
-                        <label>Spd</label>
-                        <input
-                            className="w-9 place-self-end text-right"
-                            value={pokemonInfo ? pokemonInfo?.stats[4].base_stat : 0}
-                            name="spd"
-                        />
+                        <label className='col-span-1'>Spd</label>
+                        <div className='col-span-5'>bar placeholder</div>
+                        <div className='col-span-1 place-self-end'> {pokemonInfo ? statCalculation(4) : 0} </div>
 
-                        <label>Spe</label>
-                        <input
-                            className="w-9 place-self-end text-right"
-                            value={pokemonInfo ? pokemonInfo?.stats[5].base_stat : 0}
-                            name="spe"
-                        />
+                        <label className='col-span-1'>Spe</label>
+                        <div className='col-span-5'>bar placeholder</div>
+                        <div className='col-span-1 place-self-end'> {pokemonInfo ? statCalculation(5) : 0} </div>
 
                     </div>
                 </div>
