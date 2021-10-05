384873a5a9b6bfe496df7cf4014bd038011929a5
Stone Sha <36431006+stonesha@users.noreply.github.com>
2021-10-05 06:02:00 -0700

    refactored fetching resources

--- lib/helper_functions.ts
+++ lib/helper_functions.ts
@@ -1,7 +1,54 @@
+import { PokeResponse, Option, Sprites } from '@/lib/interfaces';
+import pokedex from '@/lib/pokeapi';
+
+const pokeSpriteUrl = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/";
+const itemSpriteUrl = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/items/';
+
 export const capitalize = (str: string): string => {
     return str.charAt(0).toUpperCase() + str.slice(1);
 }
 
 export const formatLabel = (str: string): string => {
     return str.split("-").map(temp => capitalize(temp)).join(" ");
+}
+
+export const fetchResources = async () => {
+    const pokemonList: PokeResponse[] = (await pokedex.getPokemonsList()).results;
+    const naturesList: PokeResponse[] = (await pokedex.getNaturesList()).results;
+    const itemList: PokeResponse[] = (await pokedex.getItemsList()).results;
+
+    const pokemonSprites: Sprites = {};
+    const pokemonOptions: Option[] = pokemonList.map(pokemon => {
+        const name: string = pokemon.name;
+        const spriteNum: string = pokemon.url.substring(pokemon.url.slice(0, -1).lastIndexOf('/') + 1).slice(0, -1);
+        const spriteUrl: string = pokeSpriteUrl + spriteNum + ".png";
+        pokemonSprites[name] = spriteUrl;
+
+        return { value: name, label: formatLabel(name) } as Option;
+    });
+
+    const natureOptions: Option[] = naturesList.map(nature => {
+        return { value: nature.name, label: formatLabel(nature.name) } as Option;
+    });
+
+    const itemSprites: Sprites = {};
+    const itemOptions: Option[] = itemList.map(item => {
+        const name: string = item.name;
+        const spriteUrl: string = itemSpriteUrl + item.name + '.png';
+        itemSprites[name] = spriteUrl;
+
+        return { value: name, label: formatLabel(name) } as Option;
+    });
+
+    return {
+        options: {
+            pokemonOptions,
+            natureOptions,
+            itemOptions
+        },
+        sprites: {
+            pokemonSprites,
+            itemSprites
+        }
+    }
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,57 +1,21 @@
 import { GetStaticProps } from 'next'
+import { useMemo, useState } from 'react';
 
 import Container from '@/components/Container';
 import PokeParty from '@/components/PokeParty';
-import pokedex from '@/lib/pokeapi';
-import { formatLabel } from '@/lib/helper_functions';
 import { PokemonPartyContext } from '@/context/PokemonPartyContext';
-
-const pokeSpriteUrl = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/";
-const itemSpriteUrl = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/items/';
-
-import { PokeResponse, Option, Sprites, HomeProps } from '@/lib/interfaces';
-import { useMemo, useState } from 'react';
+import { fetchResources } from '@/lib/helper_functions';
+import { HomeProps } from '@/lib/interfaces';
 
 // on build time, fetch whole list of pokemon and format options for the select
 export const getStaticProps: GetStaticProps = async () => {
-	const pokemonList: PokeResponse[] = (await pokedex.getPokemonsList()).results;
-	const naturesList: PokeResponse[] = (await pokedex.getNaturesList()).results;
-	const itemList: PokeResponse[] = (await pokedex.getItemsList()).results;
 
-	const pokemonSprites: Sprites = {};
-	const pokemonOptions: Option[] = pokemonList.map(pokemon => {
-		const name: string = pokemon.name;
-		const spriteNum: string = pokemon.url.substring(pokemon.url.slice(0, -1).lastIndexOf('/') + 1).slice(0, -1);
-		const spriteUrl: string = pokeSpriteUrl + spriteNum + ".png";
-		pokemonSprites[name] = spriteUrl;
-
-		return { value: name, label: formatLabel(name) } as Option;
-	});
-
-	const natureOptions: Option[] = naturesList.map(nature => {
-		return { value: nature.name, label: formatLabel(nature.name) } as Option;
-	});
-
-	const itemSprites: Sprites = {};
-	const itemOptions: Option[] = itemList.map(item => {
-		const name: string = item.name;
-		const spriteUrl: string = itemSpriteUrl + item.name + '.png';
-		itemSprites[name] = spriteUrl;
-
-		return { value: name, label: formatLabel(name) } as Option;
-	});
+	const resources = await fetchResources();
 
 	return {
 		props: {
-			options: {
-				pokemonOptions,
-				natureOptions,
-				itemOptions
-			},
-			sprites: {
-				pokemonSprites,
-				itemSprites
-			}
+			options: resources.options,
+			sprites: resources.sprites
 		}
 	}
 }
